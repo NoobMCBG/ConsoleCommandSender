09962d7b53ec1ba9fa567abf0f154ff9513e84f5
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-26 16:57:26 +0700

    added

--- src/NoobMCBG/ConsoleCommandSender/commands/Commands.php
+++ src/NoobMCBG/ConsoleCommandSender/commands/Commands.php
@@ -24,17 +24,42 @@ class Commands extends Command implements PluginOwned {
 		if(!$this->testPermission($sender)){
 			return true;
 		}
-		if(!$sender instanceof Player){
-			$sender->sendMessage("§cYou can only use this command in-game.");
-			return true;
-		}
 		if(!isset($args[0])){
-			$sender->sendMessage("§cUsage:§7 /consolecommandsender <command>");
+			$sender->sendMessage("§cUsage:§7 /consolecommandsender help");
 			return true;
 		}
-		array_shift($args);
-		$this->getOwningPlugin()->consoleCommandSender(implode(" ", $args));
-		$sender->sendMessage("§aSuccessfully run command§d /".implode(" ", $args));
+		switch($args[0]){
+			case "help":
+			case "?":
+				$sender->sendMessage("§6> ConsoleCommandSender display commands:\n" .
+					"§b/consolecommandsender runcommand§7 - to run command in console\n" .
+					"§b/consolecommandsender about§7 - see info plugin");
+			break;
+			case "runcommand":
+			case "rca":
+			case "ra":
+			case "rcmd":
+			case "runcmd":
+				if(!$sender instanceof Player){
+					$sender->sendMessage("§cYou can only use this command in-game.");
+					return true;
+				}
+				if(!isset($args[1])){
+					$sender->sendMessage("§cUsage:§7 /consolecommandsender runcommand <command>");
+					return true;
+				}
+				array_shift($args);
+				$this->getOwningPlugin()->consoleCommandSender(implode(" ", $args));
+				$sender->sendMessage("§aSuccessfully run command§d /".implode(" ", $args));
+			break;
+			case "about":
+			case "ver":
+			case "version":
+				$sender->sendMessage("§6Console§eCommand§bSender §av" . $this->getOwningPlugin()->getDescription()->getVersion() . "\n" . 
+					"§bPlugin by:§d NoobMCBG\n" .
+					"§bDiscord:§d NoobMCBG#5862");
+			break;
+		}
 	}
 
 	public function getOwningPlugin() : Loader {
