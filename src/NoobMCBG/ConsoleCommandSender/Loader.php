df88ce2f56ecc640bc34f86883b9c4370ff8cfd9
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-06-26 16:34:53 +0700

    added
    added Loader.php
    added CheckUpdateTask.php

---
+++ src/NoobMCBG/ConsoleCommandSender/Loader.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoobMCBG\ConsoleCommandSender;
+
+use pocketmine\plugin\Pluginbase;
+use pocketmine\console\ConsoleCommandSender;
+use NoobMCBG\ConsoleCommandSender\commands\Commands;
+use NoobMCBG\ConsoleCommandSender\task\CheckUpdateTask;
+
+class Loader extends Pluginbase {
+
+	public static $instance;
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
+
+	public function onEnable() : void {
+		$this->getServer()->getCommandMap()->register("/consolecommandsender", new Commands($this));
+		$this->checkUpdate();
+		self::$instance = $this;
+	}
+
+	public function checkUpdate(bool $isRetry = false) : void {
+		$this->getServer()->getAsyncPool()->submitTask(new CheckUpdateTask($this->getDescription()->getName(), $this->getDescription()->getVersion()));
+	}
+
+	public function consoleCommandSender(string $command){
+		$this->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage()), $command);
+	}
+}
